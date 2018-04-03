eeabf40797e1541f294aace3306c1cb13d27d80b
AColossus <842236625@qq.com>
2018-04-03 18:46:53 +0800

    gitignore

--- src/test/java/org/cbb/wasteRecovery/daoTest/CollectorDaoTest.java
+++ src/test/java/org/cbb/wasteRecovery/daoTest/CollectorDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;  
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.Resource;
+
 @RunWith(value =SpringJUnit4ClassRunner.class)
 //告诉junit spring配置文件
 @ContextConfiguration(value ="classpath:spring/spring-dao.xml")
